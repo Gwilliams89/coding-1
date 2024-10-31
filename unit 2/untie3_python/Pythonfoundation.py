5475c1d6a323ba4366abc24c5dcb7bbcd08dcecf
Gwilliams89 <williamsg859@boyslatin.net>
2024-10-31 14:20:29 +0000

    Learned about python

---
+++ unit 2/untie3_python/Pythonfoundation.py
@@ -0,0 +1,38 @@
+# data types=dates are the foundation builing blocks 
+# of python
+
+# Srings characters wrapped around quotation marks
+"This is a string//22309ru"
+"The computer does not care for spelling."
+"It only cares if the charters we write ar in qoutes"
+
+# intergers whole numbers
+from sympy import false, true
+
+
+120
+11
+123
+1334143542532
+
+# float = deciaml numbers
+234.67
+986.9879
+
+# boolean=true or false value.
+true, 1
+false, 0
+
+# variable = a container for data
+#variable conssitst of three parts:
+# 1. we have a variable/container name
+# 2. We have the equl sign
+# 3. we have the value/data
+
+John= 80
+Mary= 80
+Ab= 80
+Beth= 70
+tom= 90
+
+print()
