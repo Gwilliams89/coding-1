8f622006d6c30a2f3f66dda4f0f4a1ac504572cc
Gwilliams89 <williamsg859@boyslatin.net>
2024-11-06 15:23:04 +0000

    review and added questions

---
+++ unit 2/untie3_python/syncActivity_nov1.py
@@ -0,0 +1,15 @@
+number1=200
+
+Use what you've learned about data types and variables and create two variables that when added together will result in the value 245.
+number2=45 
+
+print(number1 + number2) 
+
+
+# If I were asked to make a calander app I would use the varible data type
+
+
+ # For the date I would use the interger data type # The Interger is all whole numbers and floats are numbers with decimals
+
+
+ # the data type is boolian and print # The three things I would like to code faster, to know coding better, and make quality code
