1e3dc4b32242e634a4e39ffa19bd0ce0480cc508
Gwilliams89 <williamsg859@boyslatin.net>
2024-12-03 15:23:08 +0000

    Forgot to subment and finsh

---
+++ unit 3/functions.py
@@ -0,0 +1,39 @@
+# What is a function 
+
+# A function is a set of instruction that preform a specific 
+
+# task or job.
+
+# there are two variations of functions in python 
+
+# Built-in Functions- instructions that are pre-written in python 
+
+# programing language 
+
+# print() is a built in function. When we oass in data it will output 
+
+# it into the terminal automatically. print('Coding Class') 
+
+# input () is a built in function. It allows us to write and pass #dra into our program from the terminal name= input("what is your name?") print(name) number= int(input('please provide your pin number')) print(23^number) 
+
+# User Defined function- custom functions written by engineers 
+
+# Function Syntax- how it is written 
+
+# def is short for define
+
+# this is a function definition def sandwitcheInstructions(): print("step 1. get 2 pieces of bread,") print("step 2. putting ingredients inside of bread,") print("step 3. put bread with ingredients together,") 
+
+# there are two states of a user defined function; 
+
+# the function defintion 
+
+# the function call 
+
+# this is a function call 
+
+# 11/20/2024 
+
+# fumnctions using arithmitic operators def depositMoney(x): checkingAccount =100 print('Money has been deposited successfully') print('new checking account balance is below: ') print(x + checkingAccount) depositMoney(35) depositMoney(300) depositMoney(195) def withdrawMoney(x): checkingAccount =1000 print('Your Money has been withdrawed successfully') print('Your new balance is below') print(checkingAccount - x) withdrawMoney(5) withdrawMoney(403) withdrawMoney(67) def checkAccountBalance(): checkingAccount =2000 print('here is current check account balance') print(checkingAccount) 
+
+# def transferMoney()
---
+++ untie3_python/HnadsOnFunctions.py
@@ -0,0 +1,62 @@
+# Discuss the anatomy of a function 
+
+# A function defintion provides Instructions 
+# To the computer on what to do with data
+
+# data = just data types
+
+# () curly brackets are called parameters.
+# This is where we pass in data in the function 
+# definition 
+
+# paramte = placeholder(for data)
+
+def customizeMyName(name);
+   print('Your new custom name is Dasog '+ name)
+
+# argument is the data passed inside inside of the function 
+# call
+# argument = evidence or real facts and data
+
+customizeMyName('gian')
+
+# lesson on Conditional statements
+
+# conditional statements are built using 
+# the 'If' and 'else' keywords. They allow us
+# to make decions based on the data we receive.
+
+# a condititonal function for if someone is old enough 
+# to buy GTA VI
+
+def vertifyAge(age):
+    if age > 17:
+        print('Congrats! You can buy GTA VI')
+    else:
+        print('Sorry, you need an adult to buy this game.')
+
+vertifyAge(19)
+
+
+def playerhealth(hp) 
+    if hp > 0:
+        print('your are still alive')
+    elif hp > 100:
+        print('Congrats! you have leveled up!')
+    else:
+        print('Game over your hp is zeor')
+ 
+playerhealth(101)
+    
+
+
+
+
+
+
+
+
+
+
+
+
